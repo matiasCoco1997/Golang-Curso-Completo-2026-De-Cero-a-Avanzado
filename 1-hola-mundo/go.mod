5703d0120fecc12bdb5b25a5210a53492df9224e
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-08-19 16:23:36 -0300

    variables

--- 1-hola-mundo/go.mod
+++ 1-hola-mundo/go.mod
@@ -2,8 +2,9 @@ module holamundo
 
 go 1.26.6
 
+require rsc.io/quote v1.5.2
+
 require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
-	rsc.io/quote v1.5.2 // indirect
+	golang.org/x/text v0.39.0 // indirect
 	rsc.io/sampler v1.3.0 // indirect
 )
--- 1-hola-mundo/hola.go
+++ 1-hola-mundo/hola.go
@@ -9,4 +9,17 @@ import (
 func main() {
 	fmt.Println("Hola mundo")
 	fmt.Println(quote.Hello())
+
+	//Declaración de variables
+
+	//Como declarar una variable nueva con el simbolo ":="
+	// firstName := "Matías"
+
+	var firstName, lastName string
+	var age int = 28
+
+	firstName = "Matías"
+	lastName = "Coco"
+
+	fmt.Println(firstName, lastName, age)
 }
