3412e8071576d7aabeb66350f633db028877f8fb
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-08-20 14:35:36 -0300

    funciones

--- 0-ejercicios/2-guess-number-game/main.go
+++ 0-ejercicios/2-guess-number-game/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"time"
 )
 
@@ -26,4 +27,70 @@ func main() {
 		fmt.Println("noche")
 	}
 
+	os := runtime.GOOS
+
+	switch os {
+	case "windows":
+		fmt.Println("Windows")
+
+	case "linux":
+		fmt.Println("Linux")
+
+	case "darwin":
+		fmt.Println("MAC")
+
+	default:
+		fmt.Println("Otro OS")
+	}
+
+	//inicializando una variable dentro del switch
+	switch os2 := runtime.GOOS; os2 {
+	case "windows":
+		fmt.Println("Windows")
+
+	case "linux":
+		fmt.Println("Linux")
+
+	case "darwin":
+		fmt.Println("MAC")
+
+	default:
+		fmt.Println("Otro OS")
+	}
+
+	// Bucle for ----------------------------------
+
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+		if i == 5 {
+			break
+		}
+	}
+
+	fmt.Println(hello("matias"))
+
+	fmt.Println(suma(1, 2))
+
+	//asigno los valores a variables
+	sum, prod := calc(4, 5)
+	fmt.Println("La suma es:", sum)
+	fmt.Println("El producto es:", prod)
+
+}
+
+//funciones
+
+func hello(name string) string {
+	return "Hola, " + name
+}
+
+func suma(a, b int) int {
+	return a + b
+}
+
+// se pueden retornar mas de un valor
+func calc(a, b int) (sum, prod int) {
+	sum = a + b
+	prod = a * b
+	return
 }
